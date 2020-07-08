b12591f0f43ef0b015a313b06dceaee14ef1bcfd
Anh Truong <worldendcarthasis@gmail.com>
2020-07-08 17:58:32 -0400

    mvp

--- users/userRouter.js
+++ users/userRouter.js
@@ -2,24 +2,75 @@ const express = require('express');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  // do your magic!
+const Users = require("./userDb.js");
+const Posts = require("../posts/postDb");
+
+
+router.post('/', validateUser, (req, res) => {
+  const userBody = req.body
+  Users.insert(userBody)
+    .then((user) => {
+      res.status(201).json(user)
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({error: "There was an error in adding a new user"})
+    })
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
+  const {id} = req.params
+  const {text} = req.body
+
+  console.log({text, user_id: id})
+
+  Posts.insert({text, user_id: id})
+    .then((post) => {
+      res.status(200).json(post)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json({message: "There was an error in submitting the post"})
+    })
+
 });
 
 router.get('/', (req, res) => {
-  // do your magic!
+  Users.get()
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json({error: "There was an error in fetching users"})
+    })
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', validateUserId, (req, res) => {
+  const {id} = req.params;
+
+  Users.getById(id)
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch((err) => {
+      console.log(err);
+
+    })
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
+router.get('/:id/posts', validateUserId, (req, res) => {
+  const {id} = req.params;
+
+  Users.getUserPosts(id)
+    .then((post) => {
+      res.status(200).json(post)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json({message: "Could not get user post"})
+    })
+
 });
 
 router.delete('/:id', (req, res) => {
@@ -33,15 +84,53 @@ router.put('/:id', (req, res) => {
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  const {id} = req.params
+
+  Users.getById(id)
+    .then(user => {
+      if (user != null) {
+        req.user = user;
+        next()
+      }
+      else {
+        res.status(400).json({message: "invalid user id"})
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ message: "Cannot get user ID" });
+    });
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  const {userBody} = req.body;
+
+  if (userBody.length == 0) {
+    res.status(400).json({ message: "missing user data" });
+  }
+  else if ((userBody.name).length == 0) {
+    res.status(400).json({ message: "missing required name field" });
+  }
+  else {
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  const postText = req.body.text;
+
+  if (Object.keys(req.body).length === 0 ) {
+    res.status(400).json({message: "missing post data"})
+  }
+
+  else if ((postText).length == 0) {
+    res.status(400).json({message: "missing required text field"})
+  }
+
+  else {
+    next();
+  }
+
 }
 
 module.exports = router;
