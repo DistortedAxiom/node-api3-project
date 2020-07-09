9c0676ea035fd94040c220e40182c51517f8c377
Anh Truong <worldendcarthasis@gmail.com>
2020-07-09 15:24:37 -0400

    ready for deployment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // code away!
 const server = require("./server.js");
 
-const port = 8000
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
     console.log(`Server is running on port: ${port}`)
